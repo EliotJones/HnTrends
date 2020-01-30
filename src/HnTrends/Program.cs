70ec1d0330f425983b0103dc1afb4214d25b9b0b
Eliot Jones <elioty@hotmail.co.uk>
2020-01-30 15:39:30 +0000

    add serilog logging

--- src/HnTrends/Program.cs
+++ src/HnTrends/Program.cs
@@ -1,19 +1,30 @@
-﻿using System.IO;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-
-namespace HnTrends
+﻿namespace HnTrends
 {
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+    using Serilog;
+    using System.IO;
+    using Serilog.Events;
+
     public class Program
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File(string.Empty, LogEventLevel.Information, rollOnFileSizeLimit: true,
+                    rollingInterval:RollingInterval.Day)
+                .CreateLogger();
+
             CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args)
         {
             return WebHost.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseUrls("http://localhost:5225")
