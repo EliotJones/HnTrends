72ebfe28f0a42337ebeaceb6a131beb7389c0b8e
Eliot Jones <elioty@hotmail.co.uk>
2020-01-31 17:19:54 +0000

    ignore leading hyphen in multi word search since this implies the use of an exclude operator.

--- src/HnTrends/Controllers/HomeController.cs
+++ src/HnTrends/Controllers/HomeController.cs
@@ -83,7 +83,7 @@
             for (var i = 0; i < parts.Length; i++)
             {
                 var part = parts[i];
-                if (part.Length > 1 && part[0] != '+')
+                if (part.Length > 1 && part[0] != '+' && part[0] != '-')
                 {
                     result += '+';
                 }
